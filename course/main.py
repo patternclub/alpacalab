7a552be4f9c384b049a38c814a14b946af011402
Alex McLean <alex@slab.org>
2025-01-23 13:36:20 +0000

    twiddles

--- course/main.py
+++ course/main.py
@@ -9,7 +9,7 @@ from umqtt.simple import MQTTClient
 from mycolour import hsv_to_rgb, colour_names
 from inventor import Inventor2040W
 
-NAME = "alex"
+from config import name, wifi_name, wifi_password, mqtt_server, mqtt_user, mqtt_pass
 
 board = Inventor2040W()
 
@@ -19,39 +19,29 @@ NUM_LEDS = 66
 # How long between cheerslight updates in seconds
 INTERVAL = 60
 
-mqtt_server = "slab.org"
-mqtt_user = "alpaca"
-mqtt_pass = "dorkface"
-
-#mqtt_server = "sponge.algorithmicpattern.org"
-#mqtt_user = "alex"
-#mqtt_pass = "betweenyourtoes"
-
-
 status = {}
 
 # Check and import the SSID and Password from secrets.py
 try:
-    from secrets import WIFI_SSID, WIFI_PASSWORD
-    if WIFI_SSID == "":
-        raise ValueError("WIFI_SSID in 'secrets.py' is empty!")
-    if WIFI_PASSWORD == "":
-        raise ValueError("WIFI_PASSWORD in 'secrets.py' is empty!")
+    if wifi_name == "":
+        raise ValueError("wifi_name in 'config.py' is empty!")
+    if wifi_password == "":
+        raise ValueError("wifi_password in 'config.py' is empty!")
 except ImportError:
-    raise ImportError("'secrets.py' is missing from your Plasma 2350 W!")
+    raise ImportError("'config.py' is missing or badly formatted.")
 except ValueError as e:
     print(e)
 
-network.hostname(NAME)
+network.hostname(name)
 wlan = network.WLAN(network.STA_IF)
 
 
 def connect():
     # Connect to the network specified in secrets.py
     wlan.active(True)
-    wlan.connect(WIFI_SSID, WIFI_PASSWORD)
+    wlan.connect(wifi_name, wifi_password)
     while wlan.isconnected() is False:
-        print("Attempting connection to {}".format(WIFI_SSID))
+        print("Attempting connection to {}".format(wifi_name))
         time.sleep(1)
 
 
@@ -70,17 +60,14 @@ ip_addr = wlan.ipconfig('addr4')[0]
 
 # Let the user know the connection has been successful
 # and display the current IP address of the Plasma 2350 W
-print("Successfully connected to {}. Your Plasma 2350 W's IP is: {}".format(WIFI_SSID, ip_addr))
+print("Successfully connected to {}. Your Plasma 2350 W's IP is: {}".format(wifi_name, ip_addr))
 
 color = (255,255,255)
-
-last = time.ticks_ms()
 
 def sub_cb(topic, s_msg):
     global color, last
     topic = topic.decode('ASCII')
-    # print(topic)
-    if led_strip and (topic == "/light/all" or topic == "/light/" + NAME):
+    if led_strip and (topic == "/light/all" or topic == "/light/" + name):
         msg = ujson.loads(s_msg)
         if 'light' in msg:
             light = msg['light']
@@ -94,29 +81,23 @@ def sub_cb(topic, s_msg):
                 color = (msg['red']*255.0, msg['green']*255.0, msg['blue']*255.0)
             status[light] = color
             led_strip.set_rgb(int(light), *color)
-    elif topic == "/move/all" or topic == "/move/" + NAME:
+    elif topic == "/move/all" or topic == "/move/" + name:
         msg = ujson.loads(s_msg)
-        # print(msg)
-        if 'by' in msg:
+        if 'move' in msg:
             motor = msg['motor'] or 0
-            by = msg['by']
-            #print(by)
+            move = msg['move']
             s = board.servos[motor]
-            s.value(by)
-            foo = time.ticks_ms()
-            print(foo-last)
-            last = foo
-            
+            s.value(move)
             
 
-mqtt = MQTTClient(NAME, mqtt_server, user=mqtt_user, password=mqtt_pass)
+mqtt = MQTTClient(name, mqtt_server, user=mqtt_user, password=mqtt_pass)
 mqtt.set_callback(sub_cb)
 
 mqtt.connect()
 mqtt.subscribe("/light/all")
-mqtt.subscribe("/light/" + NAME)
+mqtt.subscribe("/light/" + name)
 mqtt.subscribe("/move/all")
-mqtt.subscribe("/move/" + NAME)
+mqtt.subscribe("/move/" + name)
 
 print("Connected to %s mqtt server" % (mqtt_server,))
 mqtt.set_callback(sub_cb)
@@ -137,3 +118,4 @@ while True:
     mqtt.check_msg()
 
 print("oh!")
+
